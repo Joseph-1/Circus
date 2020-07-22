b2f4aa8f3d72f821ff4c9b4e38248076dfca2af8
Joseph ANTHONY <anthony.joseph67150@gmail.com>
2020-07-22 11:43:24 +0200

    intégration de bootstrap + ajout navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,21 @@
 <?php
 
 
-class HomeController
-{
+namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_index")
+     */
+    public function index() : Response
+    {
+        return $this->render('home/index.html.twig', [
+            'title' => 'Bienvenue sur le Wild Circus !',
+        ]);
+    }
 }
